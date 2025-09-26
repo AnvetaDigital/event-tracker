f4afaf1c1b564fdd6f3ba3ca90854d85014de514
AnvetaDigital <nangarea2017@gmail.com>
2025-09-27 00:02:24 +0530

    [chore]: setup server and express app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,26 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import router from "./routes/authRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+dotenv.config();
 
-const PORT = 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.json({ message: "Hello from mini-event-tracker" });
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", router);
+app.use("/api/events", eventRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express.js");
 });
 
 app.listen(PORT, () => {
-    console.log(`server is running on :http://localhost:${PORT}`);
+  console.log(`server is running at: http://localhost:${PORT}`);
 });
