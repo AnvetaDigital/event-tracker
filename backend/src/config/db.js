ed8fa8e7e23df7012650593f9204e803d649cb97
AnvetaDigital <nangarea2017@gmail.com>
2025-09-27 00:03:20 +0530

    [feat]: setup MongoDB connection with Mongoose

---
+++ backend/src/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {
+    console.error(`error getting while mongoDB connection: ${error}`);
+    process.exit(1);
+  }
+};
