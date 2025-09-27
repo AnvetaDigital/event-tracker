fc888dc5363a631c3d5e96822add7d93bd2b61fa
AnvetaDigital <nangarea2017@gmail.com>
2025-09-27 11:31:29 +0530

    [feat]: add routing for all components

--- frontend/event-tracker/src/App.jsx
+++ frontend/event-tracker/src/App.jsx
@@ -1,11 +1,19 @@
+import "./index.css";
+import Dashboard from "./components/Dashboard";
+import LoginForm from "./components/LoginForm";
+import SignUp from "./components/SignupForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      mini_event_tracker_app
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
